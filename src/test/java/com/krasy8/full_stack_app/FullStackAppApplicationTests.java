0dc881d4d0228fdac182ee9c97abd0f9535c5e70
Konrad_Krasowski <krasy8@gmail.com>
2024-09-21 18:15:41 +0100

    app and db contenerized into one image

--- src/test/java/com/krasy8/full_stack_app/FullStackAppApplicationTests.java
+++ src/test/java/com/krasy8/full_stack_app/FullStackAppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.krasy8.full_stack_app;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FullStackAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.krasy8.full_stack_app;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FullStackAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
